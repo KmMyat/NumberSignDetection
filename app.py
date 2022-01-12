841884cd4cc5b4f87318bcd77923e681e944e7ba
Km <89477418+KmMyat@users.noreply.github.com>
2022-01-13 06:10:40 +0630

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def newtest():
     FRAME_WINDOW = st.image([])
     camera = cv2.VideoCapture(0)
 
-    while run:
+    while True:
         _, frame = camera.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         FRAME_WINDOW.image(frame)
